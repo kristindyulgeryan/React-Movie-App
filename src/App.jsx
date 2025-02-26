0cc68d6a24e0cca32ecf35db41f9bddf1a1a7b0d
Kristin <kristindyulgeryan@abv.bg>
2025-02-26 15:57:51 +0200

    Add movie title header and background poster

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,19 @@
 const App = ()=> {
   
 return(
-<div> 
- <h1 className="text-3xl font-bold underline">
-Hello Worl!
-  </h1>
-  </div>
+<main> 
+
+  <div className="pattern" /> 
+ <div className="wrapper">
+<header>
+
+  <img src="./hero.png" alt="Hero Banner"/>
+  <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Wihtout the Hassle</h1>
+
+</header>
+<p>Search</p>
+ </div>
+  </main>
 )
 
 }
