15858821a3b4c615b2ba56f61441ea8124f993e2
Kristin <kristindyulgeryan@abv.bg>
2025-02-27 13:39:28 +0200

    Integrate TMDb API for movie data

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,36 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Search from "./components/Search.jsx"
 
 
+const API_BASE_URL = 'https://api.themoviedb.org/3';
 
+const API_KEY =import.meta.env.VITE_TMBD_API_KEY;
+
+
+const API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    accept: 'aplication/json',
+    Authorization: `Bearer ${API_KEY}`
+  }
+
+}
 
 const App = ()=> {
-  const [searchTerm, setSearchTerm] = useState('')
+  const [searchTerm, setSearchTerm] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const fetchMovies = async () => {
+    try {
+
+    } catch(error){
+      console.error(`Error fetching movies: ${error}`)
+    }
+  }
+
+  useEffect(()=>{
+
+  }, [])
 return(
 <main> 
 
